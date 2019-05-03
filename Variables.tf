b5d7e62d438a24debbb5dc51c9c42e851cb5a273
Bumberslam <34245051+Bumberslam@users.noreply.github.com>
2019-05-03 14:07:28 -0500

    Add files via upload

---
+++ Variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+    description = "Default AWS region"
+    default = "us-east-2"
+}
+
+variable "instance" {
+    description = "Free Tier Instance"
+    default =  "t2.micro"
+}
